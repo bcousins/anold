4cf746f88a0f838cc908485277197142f1d54f01
Ben Cousins <ben.cousins8496@gmail.com>
2013-04-01 17:03:27 +1100

    A whole lotta changes and bugfixes :)

--- account/index.php
+++ account/index.php
@@ -14,12 +14,31 @@ echo '  <span class=gensmall>
             </span>
             <span class=nav>&raquo; <a href="/" class=nav>Asperger\'s Network</a> &raquo;  Edit My Account</span><br><br>';
 
-echo '<center><font size="+3">Edit My Account</font></center><br><br>';
-$userinfo = mysql_query("SELECT * from `members` where `id` = '{$_SESSION["username"]}'");
+echo '<center><font size="+2">Edit My Account</font></center><br><br>';
+if(!$_POST){
+$userinfo = mysql_query("SELECT * from `members` where `username` = '{$_SESSION["username"]}'") or die(mysql_error());
 while($row = mysql_fetch_assoc($userinfo)){
-
+echo '<center><form method="post" action="">';
+echo '<table border="0" width="75%" class="userprofile">
+	<tr>';
+echo '	<th>Username</th><td>'.$row['username'].'</td>
+	</tr>
+	<tr>
+		<th>E-Mail Address</th><td><input type="text" value="'.$row['email'].'"></td>
+	</tr>
+	<tr>
+		<th>Amusing Quip</th><td><input type="text" value="'.$row['quip'].'">
+	</tr>';
+echo '<tr>
+		<th><input type="submit" value="Update Profile"></th>
+	</tr>';
+echo '</table>
+</form></center>';
 }
-
+}
+else{
+	echo 'Must code this <a href="javascript.go(-1)">Return to form</a>';
+}
 //And in closing...
 include '../tmp/footer.php';
 ?>
--- forums/index.php
+++ forums/index.php
@@ -149,17 +149,13 @@ while($rowf = mysql_fetch_array($sqlforum1))
 		echo '</td></tr>';
 	}
 	//End the Table
-	echo '</tbody> </table></center>';
-	if($gid >= 3){
-	echo '<br><br>';
-	//Query the DB for Categories ($sqlcat), forums ($sqlfrm), Recent Posts - And much, much, much more.
+	echo '</tbody> </table></center><br><br>';
+if($gid == 3){
+
+//Query the DB for Categories ($sqlcat), forums ($sqlfrm), Recent Posts - And much, much, much more.
 $sqlcat1 = mysql_query('SELECT * FROM  `f-cat` WHERE `id` = 5') or die(mysql_error());
 $sqlforum1 = mysql_query('SELECT * FROM  `f-forums` WHERE `cat_id` = 5 ORDER BY `id`') or die(mysql_error());
 //Prepare the div (??)
-if($gid < $row['permission']){
-
-}
-else{
 while($row = mysql_fetch_array($sqlcat1))
   {
 	echo '<div class="cat">';
@@ -170,8 +166,82 @@ while($row = mysql_fetch_array($sqlcat1))
   echo '<center><table border="0" class="forums"> <tbody>';
 while($rowf = mysql_fetch_array($sqlforum1))
 	{
-		if($gid < $rowf['permission']){
-		}
+		echo '<tr><td class="f-link"><h3><a href="f-f'.$rowf["id"];
+		echo '.htm">'.$rowf["name"].'</a></h3></td>';
+		echo '<td class="forumrecent">';
+		$sqlrecentposts = mysql_query("SELECT * FROM `f-topics` where `f_id` = '{$rowf["id"]}' && `deleted` = 0 ORDER BY `time` DESC LIMIT 3") or die(mysql_error());
+			while($rowrecent = mysql_fetch_array($sqlrecentposts)){
+			if($rowrecent['deleted'] == 1){
+			echo '<a href="f-del-t'.$rowrecent['id'].'.htm">'.$rowrecent['title'];
+			echo '</a><br>';
+				}
+				else{
+			echo '<a href="f-t'.$rowrecent['id'].'.htm">'.$rowrecent['title'];
+			echo '</a><br>';
+				}
+
+			}
+		echo '</td></tr>';
+	}
+	//End the Table
+	echo '</tbody> </table></center><br><br>';
+}
+
+elseif($gid >= 5){
+
+//Query the DB for Categories ($sqlcat), forums ($sqlfrm), Recent Posts - And much, much, much more.
+$sqlcat1 = mysql_query('SELECT * FROM  `f-cat` WHERE `id` = 5') or die(mysql_error());
+$sqlforum1 = mysql_query('SELECT * FROM  `f-forums` WHERE `cat_id` = 5 ORDER BY `id`') or die(mysql_error());
+//Prepare the div (??)
+while($row = mysql_fetch_array($sqlcat1))
+  {
+	echo '<div class="cat">';
+  	echo $row['name'];
+  	echo '</div>';
+  }
+  //Prepare Table
+  echo '<center><table border="0" class="forums"> <tbody>';
+while($rowf = mysql_fetch_array($sqlforum1))
+	{
+		echo '<tr><td class="f-link"><h3><a href="f-f'.$rowf["id"];
+		echo '.htm">'.$rowf["name"].'</a></h3></td>';
+		echo '<td class="forumrecent">';
+		$sqlrecentposts = mysql_query("SELECT * FROM `f-topics` where `f_id` = '{$rowf["id"]}' && `deleted` = 0 ORDER BY `time` DESC LIMIT 3") or die(mysql_error());
+			while($rowrecent = mysql_fetch_array($sqlrecentposts)){
+			if($rowrecent['deleted'] == 1){
+			echo '<a href="f-del-t'.$rowrecent['id'].'.htm">'.$rowrecent['title'];
+			echo '</a><br>';
+				}
+				else{
+			echo '<a href="f-t'.$rowrecent['id'].'.htm">'.$rowrecent['title'];
+			echo '</a><br>';
+				}
+
+			}
+		echo '</td></tr>';
+	}
+	//End the Table
+	echo '</tbody> </table></center><br><br>';
+}
+
+if($gid >= 4){
+
+//Query the DB for Categories ($sqlcat), forums ($sqlfrm), Recent Posts - And much, much, much more.
+$sqlcat1 = mysql_query('SELECT * FROM  `f-cat` WHERE `id` = 6') or die(mysql_error());
+$sqlforum1 = mysql_query('SELECT * FROM  `f-forums` WHERE `cat_id` = 6 ORDER BY `permission`') or die(mysql_error());
+//Prepare the div (??)
+while($row = mysql_fetch_array($sqlcat1))
+  {
+	echo '<div class="cat">';
+  	echo $row['name'];
+  	echo '</div>';
+  }
+  //Prepare Table
+  echo '<center><table border="0" class="forums"> <tbody>';
+  if($gid < $rowf['permission']){}
+while($rowf = mysql_fetch_array($sqlforum1))
+	{
+		if($gid < $rowf['permission']){}
 		else{
 		echo '<tr><td class="f-link"><h3><a href="f-f'.$rowf["id"];
 		echo '.htm">'.$rowf["name"].'</a></h3></td>';
@@ -189,12 +259,12 @@ while($rowf = mysql_fetch_array($sqlforum1))
 
 			}
 		echo '</td></tr>';
-		}
+	}
 	}
 	//End the Table
 	echo '</tbody> </table></center>';
 }
-}
+
 echo '</div>';
 //And in closing...
 include '../tmp/footer.php';
